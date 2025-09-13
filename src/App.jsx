8bfa9f47d7f8c97dddcf91c3871e81b4e9ae1664
Sanskar-Rijal <rijalsanskar@gmail.com>
2025-09-13 13:22:40 +0545

    fixed bug, when user loads the page blank screen comes

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import City from "./components/City/City";
 import Form from "./components/Form/Form";
 import { CitiesProvider } from "./contexts/CitiesContexts";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 export default function App() {
   return (
@@ -25,7 +26,14 @@ export default function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="/" element={<Homepage />} />
             {/* nested route for app , all of the nested routes should be displayed at sidebar*/}
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route path="cities" element={<CityList />} />
 
               {/* creating new route for storing data in URL  */}
---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect } from "react";
+
+export default function ProtectedRoute({ children }) {
+  //check if user is authenticated
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!isAuthenticated) {
+        navigate("/");
+      }
+    },
+    [isAuthenticated, navigate]
+  );
+
+  return isAuthenticated ? children : null;
+}
